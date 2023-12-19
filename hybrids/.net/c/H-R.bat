92d4d2ef87d3d5a09488ffd68975d51035038f53
Jose Iraheta Portillo <joseportillo@kpmg.com>
2023-12-19 16:36:49 -0600

    initial H-W script

---
+++ hybrids/.net/c/H-R.bat
@@ -0,0 +1,12 @@
+@echo off
+
+@rem Edge
+
+@rem Chrome
+start "" "chrome.exe" -kiosk "https://hero-wars.com/"
+@rem start "" "chrome.exe" -kiosk "https://hero-wars.com/"
+
+@rem Firefox
+start "H-R J-Script" "firefox.exe" -kiosk "https://hero-wars.com/"
+@rem new tab on the existing open browser
+@rem start "" "firefox.exe" -new-tab "https://hero-wars.com/"
